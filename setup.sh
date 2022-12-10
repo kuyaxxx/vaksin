c3e1ec4b9b9eff11326b9599bc8698b9650756e5
N.C.R <64447096+kuyaxxx@users.noreply.github.com>
2022-12-10 12:55:51 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -146,7 +146,7 @@ rm -f /root/ohp.sh
 cat <<EOF> /etc/systemd/system/autosett.service
 [Unit]
 Description=autosetting
-Documentation=https://t.me/Akbar218
+Documentation=https://t.me/KuyaxBatox
 
 [Service]
 Type=oneshot
